d6852218edd23167f6cdeceac869ff8252d0e378
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-08-20 11:33:35 -0700

    coverage of basic functional programming, next is currying

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log("Hello World!");
+import { compose, pipe } from 'lodash/fp';
+
+//trim
+const trim = str => str.trim();
+//wrap in div
+const wrap = (type, str) => `<${type}> + ${str} + </${type}>`;
+const toLowerCase = str => str.toLowerCase();
+
+//lodash Functional Programming Helpers
+//compose, pipe
+
+//compose must be used in the order that the functions wrap
+// const transform = compose(wrap, toLowerCase, trim);
+
+//pipe will read in the order that the functions will be called
+const transform2 = pipe(trim, toLowerCase, wrap);
+
+//original wrapping of functions
+const result = wrapInDiv(toLowerCase(trim(input)));
